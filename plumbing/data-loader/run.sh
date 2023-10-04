55bfafea37420aa6ed5eb0cf8a6a4c6b2aef7b6a
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-10-04 14:26:50 +0000

    Fixed loading of iot data

--- plumbing/data-loader/run.sh
+++ plumbing/data-loader/run.sh
@@ -8,7 +8,9 @@ mc mb minio/iris
 mc mb minio/kafka-checkpoint
 mc mb minio/kafka-output
 mc mb minio/stocks
+mc mb minio/iot-telemetry-data
 mc cp ./data/heart.csv minio/heart/heart.csv
 mc cp ./data/iris.csv minio/iris/iris.csv
 mc cp ./data/cities.csv minio/cities/cities.csv
 mc cp ./data/stocks.csv minio/stocks/stocks.csv
+mc cp ./data/iot-telemetry-data.csv minio/iot-telemetry-data/iot-telemetry-data.csv
